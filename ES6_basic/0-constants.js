2d072bf2aa817ad28cd20a8b580f3e2af6275420
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-11-17 17:25:11 +0100

    works on my side

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,32 +1,15 @@
-/**
- * Function to return a constant string.
- * Uses const to declare the task variable.
- *
- * @returns {string} The string 'I prefer const when I can.'
- */
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  /**
-   * Function to return a string.
-   *
-   * @returns {string} The string ' is okay'
-   */
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  /**
-   * Function to return a combined string.
-   * Uses let to declare the combination variable and modifies it.
-   *
-   * @returns {string} The combined string 'But sometimes let is okay'
-   */
-  export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
-    return combination;
-  }
-  
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
