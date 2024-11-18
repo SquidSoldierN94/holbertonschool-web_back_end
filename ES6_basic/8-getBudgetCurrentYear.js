9e97302305f0334833e8a36628eeb1dac940154a
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-11-18 16:15:54 +0100

    Pathologic is Genius, And Here's Why

---
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,16 @@
+function getCurrentYear() {
+    return 2021;
+  }
+  
+  export default function getBudgetForCurrentYear(income, gdp, capita) {
+    const currentYear = getCurrentYear();
+    
+    const budget = {
+      [`income-${currentYear}`]: income,
+      [`gdp-${currentYear}`]: gdp,
+      [`capita-${currentYear}`]: capita,
+    };
+  
+    return budget;
+  }
+  
