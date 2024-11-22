4ff5167cb1a62942ff72e010f426920a5ea953d6
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-11-22 10:02:08 +0100

    dogs in space ??

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,16 +1,15 @@
 function getCurrentYear() {
-    return 2021;
-  }
-  
-  export default function getBudgetForCurrentYear(income, gdp, capita) {
-    const currentYear = getCurrentYear();
-    
-    const budget = {
-      [`income-${currentYear}`]: income,
-      [`gdp-${currentYear}`]: gdp,
-      [`capita-${currentYear}`]: capita,
-    };
-  
-    return budget;
-  }
-  
+  return 2021;
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const currentYear = getCurrentYear();
+
+  const budget = {
+    [`income-${currentYear}`]: income,
+    [`gdp-${currentYear}`]: gdp,
+    [`capita-${currentYear}`]: capita,
+  };
+
+  return budget;
+}
