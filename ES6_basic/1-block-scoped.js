32b677a5ee71da01f4efcdc740fad5483f5085a6
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-11-22 10:19:19 +0100

    i have severe mental illnesses

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,7 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    let task = true;
   }
 
   return [task, task2];
