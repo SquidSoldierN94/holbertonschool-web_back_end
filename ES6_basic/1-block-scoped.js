5cb62509a45118b4562f209673f06eade24f5e50
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-11-22 08:28:55 +0100

    there is positive in everything i guess

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,20 +1,16 @@
 /**
- * Function to demonstrate block scoping with let and const
+ * Function to demonstrate block scoping with const
  * @param {boolean} trueOrFalse - A boolean parameter to control the if block execution
  * @returns {Array} - An array containing the values of task and task2
  */
 export default function taskBlock(trueOrFalse) {
-  // Declare task and task2 using let to ensure block scoping within the function
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    // Declare new block-scoped constants within the if block
-    // These will not overwrite the outer task and task2 variables
     const task = true;
     const task2 = false;
   }
 
-  // Return the outer scoped variables task and task2
   return [task, task2];
 }
