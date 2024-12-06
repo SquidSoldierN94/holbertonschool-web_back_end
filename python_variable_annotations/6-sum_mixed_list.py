ed2b4706fe2db611ae17bd85296b4fee86755e10
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-12-06 09:24:54 +0100

    e

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
+
+"""
+This module `sum_mixed_list` which computes the sum of a list
+that contains both point numbers. It demonstrates the use of
+type `sum()` function, and returns the sum as a float.
+"""
+
 from typing import List, Union
+
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Sums all the elements in a list of integers and floats.
 
-    This function takes a list containing both integers and floating-point numbers
+    Thisa list containing both integers and floating-point numbers
     and returns their sum as a float.
 
     Parameters:
     mxd_lst (List[Union[int, float]]): A list of integers and/or floats to sum.
 
     Returns:
-    float: The sum of all the numbers in the input list, represented as a float.
+    float: The sum of the numbers in the input list, represented as a float.
     """
     return float(sum(mxd_lst))
