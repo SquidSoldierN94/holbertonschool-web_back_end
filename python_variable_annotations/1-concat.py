218da5df54ab8db6db4ecd262083fb7e00976da4
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-12-06 09:00:10 +0100

    get it

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 """
-This module contains a function `concat` that takes two string values 
-as arguments and returns their concatenation as a single string. 
+This module contains a function `concat` that takes two string values
+as arguments and returns their concatenation as a single string.
 This module demonstrates type annotations for string operations in Python.
 """
+
 
 def concat(str1: str, str2: str) -> str:
     """
