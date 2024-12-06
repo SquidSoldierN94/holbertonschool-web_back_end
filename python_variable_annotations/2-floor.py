66aa1c7e8a67fec6665199897ea01c6ef80bc4c6
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-12-06 09:31:59 +0100

    e

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
 
-import math
+"""
+This module provides a function the floor of a given float number.
+It uses the the largest integer less than or equal to the provided float.
+The module demonstrates type annotations for better code clarity.
+"""
 
-"""
-This module contains a function `floor` that takes a float value
-as an argument and returns the floor of that float.
-It demonstrates the use of the math module and type annotations.
-"""
+
+import math
 
 
 def floor(n: float) -> int:
+
     """
     Returns the floor of the given float number.
 
-    The floor of a number is the largest integer less than or equal
-    to the number. For example, the floor of 3.14 is 3.
+    The floor of than or equal to the number.
+    For example, the floor of 3.14 is 3.
 
     Parameters:
-    n (float): The number to compute the floor value of.
+    n (float): The number for which the floor value is computed.
 
     Returns:
     int: The floor value of the input float.
